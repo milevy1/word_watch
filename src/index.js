eddd503a32626f2d0615333045ec8e6365ef7b03
Matt Levy <milevy1@gmail.com>
2019-07-18 10:22:53 -0600

    Deletes unused comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
-
-  // This code runs when the document is ready
-
   // fetch API for the top word
   // update value of id with return of fetch
   $.get("https://wordwatch-api.herokuapp.com/api/v1/top_word", function(data, status){
