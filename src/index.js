b34b5af999e110e9e1702e260189c80ce338cfa5
Matt Levy <milevy1@gmail.com>
2019-07-18 09:36:28 -0600

    Adds pseudocode for fetching the top word for the page

--- src/index.js
+++ src/index.js
@@ -2,4 +2,20 @@ import $ from 'jquery'
 
 $(document).ready(() => {
   // have fun!
+
+  // This code runs when the document is ready
+  // console.log("Hello Matt!");
+
+  // fetch API for the top word
+  fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    let topWord = JSON.stringify(myJson));
+    // update value of id with return of fetch
+    $("#fetch-top-word").text(topWord)
+  });
+
+
 })
